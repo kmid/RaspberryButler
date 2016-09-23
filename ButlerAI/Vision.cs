25b2b8f6070c265b7d4b500eb4617899722ea41f
kmid <kennethmd@gmail.com>
2016-09-23 11:06:22 +0200

    Removed packages from UI and restructured folders

---
+++ ButlerAI/Vision.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.VisualStyles;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+using System.Drawing;
+
+
+
+namespace ButlerAI
+{
+    
+    /// <summary>
+    /// Captures frames from either a webcam or a video stream avi
+    /// </summary>
+    public class Vision
+    {
+        private Capture captureDevice = null;
+
+
+        public Vision()
+        {
+            captureDevice = new Capture(@"C:\per\billy.mp4");
+        }
+        
+        /// <summary>
+        /// Get a list of areas containing detected faces 
+        /// </summary>
+        public Bitmap GetFaces()
+        {
+            var frame = captureDevice.QueryFrame();
+            return frame.ToImage<Gray,byte>().ToBitmap();
+        }
+
+        public void FrameGrab()
+        {
+            
+        }
+    }
+
+    
+}
