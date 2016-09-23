7f43d3296b44aa99d74531c0799ca47d91971b19
kmid <kennethmd@gmail.com>
2016-09-23 10:15:47 +0200

    First commit on dev

---
+++ ButlerAI/ButlerAI/Vision.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.VisualStyles;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+using System.Drawing;
+
+using Emgu
+
+
+namespace ButlerAI
+{
+    public class Vision
+    {
+        private Capture captureDevice = null;
+        
+
+
+        public Vision()
+        {
+            captureDevice = new Capture(@"C:\per\billy.mp4");
+            
+
+        }
+        
+        /// <summary>
+        /// Get a list of areas containing detected faces 
+        /// </summary>
+        public Bitmap GetFaces()
+        {
+            var frame = captureDevice.QueryFrame();
+            return frame.ToImage<Gray,byte>().ToBitmap();
+        }
+    }
+
+    
+}
---
+++ ButlerUI/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoorButler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ButlerAI.Vision a = new ButlerAI.Vision();
+            var i = a.GetFaces();
+            pictureBox1.Image = i;
+
+            //i = a.GetFaces();
+            //pictureBox1.Image = i;
+
+            //i = a.GetFaces();
+            //pictureBox1.Image = i;
+
+        }
+    }
+}
